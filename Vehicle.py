f7e809e4a6f0d21e21a2b6b27f5632ab02346070
ebbifhn <fhnebbi@gmail.com>
2022-11-29 18:51:00 -0700

    cars

--- Vehicle.py
+++ Vehicle.py
@@ -17,14 +17,3 @@ class Vehicle:
     def paint_vehicle(self, newcolor):
         self.color = newcolor
 
-bug_object = Vehicle("beetle", "yellow", 4, 1) #object of vehicle class -- instance of vehicle class
-turtle_object = Vehicle("turtlebot", "green", 2, 5) 
-rover_object = Vehicle("rover", "purple", 4, 25)
-
-bug_object.print_details()
-turtle_object.print_details()
-rover_object.print_details()
-
-bug_object.paint_vehicle("blue")
-
-bug_object.print_details()
